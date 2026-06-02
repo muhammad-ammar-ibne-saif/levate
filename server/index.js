97a6c4fc0c4e937da9db20cfbcf57c5b79ea588e
muhammad-ammar-ibne-saif <mammar6058@gmail.com>
2026-06-03 00:21:20 +0500

    updated dns

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const dns = require("dns");
-dns.setDefaultResultOrder("ipv4first");
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
+// const dns = require("dns");
+// dns.setDefaultResultOrder("ipv4first");
+// dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 require("dotenv").config({ path: "./.env" });
 
